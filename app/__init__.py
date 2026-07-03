f5866c859a3a361824e59f2f3b33186792dcb4af
matchday-brain <jburgoine@gmail.com>
2026-07-03 13:28:18 +0100

    Delete app directory

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "change-this-in-production"
-    from .routes import bp
-    app.register_blueprint(bp)
-    return app
